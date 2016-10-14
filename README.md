0a9ae2449b99a4fc41e07c90ced652590a533121
Amer Hamdan <amashamdan@gmail.com>
2016-10-14 14:33:54 -0700

    Initial commit.
    express and mongodb modules installed.

---
+++ README.md
@@ -0,0 +1,5 @@
+### Voting Vortex App
+
+Under construction.
+
+An app where the user can vote on, view and add polls.
---
+++ server.js
@@ -0,0 +1,38 @@
+/* load needed packages/ */
+var express = require("express");
+var mongodb = require("mongodb");
+var app = express();
+
+var MongoClient = mongodb.MongoClient;
+/* The url to connect to the database is saved in environment variables. */
+var mongoUrl = process.env.SEARCH_HISTORY;
+
+// port 3000 used for localhost during development.
+var port = Number(process.env.PORT || 3000)
+app.listen(port);
+
+
+/*
+function connectToMongo(status, res, itemSearched, date) {
+	MongoClient.connect(mongoUrl, function(err, db) {
+		if (err) {
+			console.log("Error connecting to database: " + err);
+		} else {
+			console.log("connection established");
+			var searchedItems = db.collection("searched_items");
+			if (status == "save") {
+					searchedItems.insert({
+					"item searched": itemSearched,
+					"time searched": date
+				});
+			} else if (status == "load") {
+				searchedItems.find({}, {"_id": false}).sort({"_id": -1}).limit(10).toArray(function(err, result) {
+					res.send(result);
+					res.end();
+				})
+			}
+		}
+	});
+}
+*/
+
