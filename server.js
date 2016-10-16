771f5c1c4a95404c799beec03fa451d0a11cbd0a
Amer Hamdan <amashamdan@gmail.com>
2016-10-16 15:43:19 -0700

    Feat: Added get request handler for /test.
    Handler to load data from database and send it to the client.
    This route will send to the user the existing polls.

--- server.js
+++ server.js
@@ -3,41 +3,32 @@ var express = require("express");
 var mongodb = require("mongodb");
 var app = express();
 
-//var MongoClient = mongodb.MongoClient;
+var MongoClient = mongodb.MongoClient;
 /* The url to connect to the database is saved in environment variables. */
-//var mongoUrl = process.env.SEARCH_HISTORY;
+var mongoUrl = process.env.VOTE;
+console.log(mongoUrl);
 
 app.get("/", function(req, res) {
 	app.use("/stylesheets", express.static(__dirname + "/stylesheets"));
 	res.sendFile(__dirname + "/index.html");
 })
 
+app.get("/test", function(req, res) {
+	MongoClient.connect(mongoUrl, function(err, db) {
+		if (err) {
+			res.send("Error connecting to database: " + err);
+			res.end();
+		} else {
+			console.log("connection established");
+			var polls = db.collection("polls");
+			polls.find({}).toArray(function(err, result) {
+					res.send(result);
+					res.end();
+			})
+		}
+	});	
+})
+
 // port 3000 used for localhost during development.
 var port = Number(process.env.PORT || 3000)
 app.listen(port);
-
-
-/*
-function connectToMongo(status, res, itemSearched, date) {
-	MongoClient.connect(mongoUrl, function(err, db) {
-		if (err) {
-			console.log("Error connecting to database: " + err);
-		} else {
-			console.log("connection established");
-			var searchedItems = db.collection("searched_items");
-			if (status == "save") {
-					searchedItems.insert({
-					"item searched": itemSearched,
-					"time searched": date
-				});
-			} else if (status == "load") {
-				searchedItems.find({}, {"_id": false}).sort({"_id": -1}).limit(10).toArray(function(err, result) {
-					res.send(result);
-					res.end();
-				})
-			}
-		}
-	});
-}
-*/
-
