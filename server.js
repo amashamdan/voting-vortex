30d2dcec8c32dc96322a11c7aa6115417476053a
Amer Hamdan <amashamdan@gmail.com>
2016-10-17 12:33:03 -0700

    Refactor: Moved all requests inside MongoClient.connect function.
    All get requests are moved inside the mongo connect callback function.
    This is done to speed up the process of loading pages.
    Once the server is started, the connection to the database is made.
    This eliminated the need to connect to the database for each request.

--- server.js
+++ server.js
@@ -10,34 +10,38 @@ console.log(mongoUrl);
 
 app.use("/stylesheets", express.static(__dirname + "/views/stylesheets"));
 
-app.get("/", function(req, res) {
-	res.sendFile(__dirname + "/views/index.html");
-})
+MongoClient.connect(mongoUrl, function(err, db) {
+	if (err) {
+		res.send("Error connecting to database: " + err);
+		res.end();
+	} else {
+		console.log("connected");
+		var polls = db.collection("polls");
 
-app.get("/home", function(req, res) {
-	MongoClient.connect(mongoUrl, function(err, db) {
-		if (err) {
-			res.send("Error connecting to database: " + err);
-			res.end();
-		} else {
-			console.log("connection established");
-			var polls = db.collection("polls");
+		app.get("/", function(req, res) {
+			res.sendFile(__dirname + "/views/index.html");
+		})
+
+		app.get("/home", function(req, res) {
+			
 			polls.find({}).toArray(function(err, result) {
-					res.send(result);
-					res.end();
+				res.send(result);
+				res.end();
+			});
+		});
+
+		app.get("/polls/:poll", function(req, res) {
+			
+			polls.find({"name": req.params.poll}).toArray(function(err, result) {
+					console.log(result);
+					var votes = result[0].votes;
+					res.locals = {poll: req.params.poll, votes: votes};
+					res.render('poll.ejs');
 			})
-		}
-	});	
-})
-
-app.get("/polls/:poll", function(req, res) {
-	var votes = 55;
-	res.locals = {poll: req.params.poll, votes: votes};
-	res.render('poll.ejs');
-})
-
-
+		})
+	}
+});
 
 // port 3000 used for localhost during development.
-var port = Number(process.env.PORT || 8081)
+var port = Number(process.env.PORT || 8083)
 app.listen(port);
