8405c6bf0a6aac7666555b8244f28f2693bf5212
Amer Hamdan <amashamdan@gmail.com>
2016-10-17 16:07:26 -0700

    Refactor: Changed data sent to client from root and changed lookup method of polls.
    root: Instead of sending all data to index.html, only id, name and votes of each poll are sent.
    votes is removed from database, so countVotes folder finds the total number of votes in each poll now.
    on /polls/:poll route: Poll is now looked up based on id not the name.
    name, list of options and number of votes are saved as locals for ejs.

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 /* load needed packages/ */
 var express = require("express");
 var mongodb = require("mongodb");
+var ObjectId = require('mongodb').ObjectID;
 var app = express();
 
 var MongoClient = mongodb.MongoClient;
 /* The url to connect to the database is saved in environment variables. */
 var mongoUrl = process.env.VOTE;
-console.log(mongoUrl);
 
 app.use("/stylesheets", express.static(__dirname + "/views/stylesheets"));
 
@@ -15,7 +15,6 @@ MongoClient.connect(mongoUrl, function(err, db) {
 		res.send("Error connecting to database: " + err);
 		res.end();
 	} else {
-		console.log("connected");
 		var polls = db.collection("polls");
 
 		app.get("/", function(req, res) {
@@ -23,25 +22,40 @@ MongoClient.connect(mongoUrl, function(err, db) {
 		})
 
 		app.get("/home", function(req, res) {
-			
 			polls.find({}).toArray(function(err, result) {
-				res.send(result);
+				var polls = [];
+				for (var entry in result) {
+					var poll = {};
+					poll.name = result[entry].name;
+					poll.votes = countVotes(result[entry].options);
+					poll.id = result[entry]["_id"];
+					polls.push(poll);
+				}
+				res.send(polls);
 				res.end();
 			});
 		});
 
 		app.get("/polls/:poll", function(req, res) {
-			
-			polls.find({"name": req.params.poll}).toArray(function(err, result) {
-					console.log(result);
-					var votes = result[0].votes;
-					res.locals = {poll: req.params.poll, votes: votes};
+			polls.find({"_id": ObjectId(req.params.poll)}).toArray(function(err, result) {
+					var name = result[0].name;
+					var list = result[0].options;
+					var votes = countVotes(list);
+					res.locals = {name: name, poll: req.params.poll, votes: votes, list: list};
 					res.render('poll.ejs');
 			})
 		})
 	}
 });
 
+function countVotes(list) {
+	var votes = 0;
+	for (var key in list) {
+		votes += list[key];
+	}
+	return votes;
+}
+
 // port 3000 used for localhost during development.
-var port = Number(process.env.PORT || 8083)
+var port = Number(process.env.PORT || 8085)
 app.listen(port);
